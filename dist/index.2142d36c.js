85bbb56482abd6f578dc289615a2f94596d27f9c
GavinL04 <71574591+GavinL04@users.noreply.github.com>
2021-03-24 18:06:26 -0400

    lowkey framework

--- dist/index.2142d36c.js
+++ dist/index.2142d36c.js
@@ -442,7 +442,46 @@ id) /*: string*/
 }
 
 },{}],"3L8AI":[function(require,module,exports) {
-console.log("ciao");
+//let score : ??
+
+[
+  {
+    questionContent: "Some random question",
+    /*     choices: ["answer #1", "answer #2", "answer #3", "answer #4"],
+    correctChoice: "answer #2", */
+    choices: [
+      {
+        choice: "answer #1",
+        correct: true,
+        value: -3,
+      },
+      {
+        choice: "answer #2",
+        correct: false,
+      },
+    ],
+  },
+];
+
+//listen for start event "click" or "page load"
+
+//display question
+
+//listen for user submit event
+
+//if answer is true then display correct modal and update score
+//else display incorrect modal and update score
+
+//if question number < questions.length display next question
+//else display results
+
+const DOMSelectors = {
+  start: document.getElementById("start"),
+  choice1: document.querySelector("choice1"),
+};
+const init = function () {
+  DOMSelectors.start.addEventListener("click", function () {});
+};
 
 },{}]},["21c8X","3L8AI"], "3L8AI", "parcelRequired264")
 
