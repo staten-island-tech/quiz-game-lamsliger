2113089233fce9c21e3c3b91bd5d099c3cdf249c
ryan-sliger <ryan.sliger22@sitechhs.com>
2021-04-12 10:01:03 -0400

    in class changes

--- dist/index.2142d36c.js
+++ dist/index.2142d36c.js
@@ -442,59 +442,62 @@ id) /*: string*/
 }
 
 },{}],"3L8AI":[function(require,module,exports) {
-//let score : ??
-
-[
-  {
-    questionContent: "Some random question",
-    /*     choices: ["answer #1", "answer #2", "answer #3", "answer #4"],
-    correctChoice: "answer #1", */
-    choices: [
-      {
-        choice: "answer #1",
-        correct: true,
-        value: +1,
-      },
-      {
-        choice: "answer #2",
-        correct: false,
-        value: -1,
-      },
-      {
-        choice: "answer #3",
-        correct: false,
-        value: -1,
-      },
-      {
-        choice: "answer #4",
-        correct: false,
-        value: -1,
-      },
-    ],
-  },
-];
-
-//listen for start event "click" or "page load"
-
-document.getElementById('btn').addEventListener('click')
-
-//display question
-
-//listen for user submit event
-
-//if answer is true then display correct modal and update score
-//else display incorrect modal and update score
-
-//if question number < questions.length display next question
-//else display results
-
-const DOMSelectors = {
-  start: document.getElementById("start"),
-  choice1: document.querySelector("choice1"),
-};
-const init = function () {
-  DOMSelectors.start.addEventListener("click", function () {});
-};
+ function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        const element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+  
+        // show options
+        const choices = quiz.getQuestionIndex().choices;
+        for(const i = 0; i < choices.length; i++) {
+            const element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+  
+        showProgress();
+    }
+  };
+   
+  function guess(id, guess) {
+    const button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+  };
+  
+  function showProgress() {
+    const currentQuestionNumber = quiz.questionIndex + 1;
+    const element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+  };
+  
+  function showScores() {
+    const gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    const element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+  };
+  
+  // create questions
+  const questions = [
+    new Question("What was the first commercially successful video game? ", ["Tank", "Pac man","Pong", "Donkey Kong"], "Pong"),
+    new Question("What is the best selling videogame of all time? ", ["Mario Kart", "Minecraft", "PUB G", "GTA V"], "Minecraft"),
+    new Question("What year was the Super Nintendo Entertainment System (SNES) released? ", ["1990", "2010","1987", "1996"], "1990"),
+    new Question("What is the highest-selling gaming console to date? ", ["Xbox 360", "Nintendo Gamecube", "Nintendo Wii", "PS 2"], "PS 2"),
+    new Question("What year was Nintendo founded? ", ["1889", "1907", "1883", "1896"], "1889"),
+    
+  ];
+  // create quiz
+  const quiz = new Quiz(questions);
+  
+  // display quiz
+  populate();
 
 },{}]},["21c8X","3L8AI"], "3L8AI", "parcelRequired264")
 
